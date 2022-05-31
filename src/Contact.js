dfc5d99e13bbe88ad56d283ed1f9791b48fa529c
cmw-dev <chelsea@webercopy.com>
2022-05-31 14:44:16 -0500

    resolved update to function names for components

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Contact.css";
 
-export default function () {
+export default function Contact() {
   return (
     <span className="contact">
       <a
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 
-export default function () {
+export default function Dictionary() {
   let [keyWord, setKeyword] = useState("");
   function search(event) {
     event.preventDefault();
