8bb41de33230ff5995adb0237e205f9926c542a9
cmw-dev <chelsea@webercopy.com>
2022-05-31 15:33:29 -0500

    container styling update

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,7 +6,7 @@ export default function Dictionary() {
   let [keyWord, setKeyword] = useState("");
 
   function handleResponse(response) {
-    console.log(response.data);
+    console.log(response.data[0]);
   }
 
   function search(event) {
@@ -33,7 +33,6 @@ export default function Dictionary() {
           autoComplete="off"
           onChange={handleKeywordChange}
         />
-
         <button>🔎</button>
       </form>
     </div>
