6fb51229e2790211eb93ac1fd463a8cd8768f042
cmw-dev <chelsea@webercopy.com>
2022-06-03 09:41:18 -0500

    added photos pexels api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyWord, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
@@ -17,9 +19,23 @@ export default function Dictionary(props) {
     search();
   }
 
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f9170000100000169cbd1eb88a2474fb770a080983e042d";
+
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyWord}&per_page=1`;
+    axios
+      .get(pexelsApiUrl, {
+        headers: { Authorization: `Bearer ${pexelsApiKey}` },
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -54,6 +70,7 @@ export default function Dictionary(props) {
             suggested words: sunset, wine, yoga, plant...
           </div>
           <Results results={results} />
+          <Photos photos={photos} />
         </div>
       </div>
     );
