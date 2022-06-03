37e1da3a5464fafff15dbccf028e02abaf5fe8a1
cmw-dev <chelsea@webercopy.com>
2022-06-03 13:11:37 -0500

    update to loop

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Dictionary defaultKeyword="moon" />
+        <Dictionary defaultKeyword="galaxy" />
         <Contact />
       </div>
     </div>
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,14 +1,18 @@
 import React from "react";
 
 export default function Phonetic(props) {
-  return (
-    <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer" alt="">
-        <i class="fa-solid fa-circle-play"></i>
-      </a>{" "}
-      {""}
-      {props.phonetic.text}
-      <br />
-    </div>
-  );
+  if (props.phonetic.audio) {
+    return (
+      <div className="Phonetic">
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+          <i class="fa-solid fa-circle-play"></i>
+        </a>{" "}
+        {""}
+        {props.phonetic.text}
+        <br />
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
